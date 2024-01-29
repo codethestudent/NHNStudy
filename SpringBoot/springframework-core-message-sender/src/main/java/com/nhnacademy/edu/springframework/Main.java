0e5b5b6057f33b72d435aba82fc219716452d50e
codeTheStudent <hjun303@gnu.ac.kr>
2024-01-29 20:37:12 +0900

    first commit

---
+++ SpringBoot/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.edu.springframework;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("aaaa@gmail.com", "010-2222-2222");
+    }
+}
