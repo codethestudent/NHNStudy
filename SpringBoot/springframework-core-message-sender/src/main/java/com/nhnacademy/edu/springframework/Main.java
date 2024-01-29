e72abc504f117c21bf6667b8a3465454e5f48b5d
codeTheStudent <hjun303@gnu.ac.kr>
2024-01-29 21:00:03 +0900

    Spring 실습 1번 복습
    ClassPathXmlApplicationContext를 이용해 bean.xml파일을 로딩하고 Spring Bean객체를 생성

--- SpringBoot/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ SpringBoot/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -1,9 +1,13 @@
 package com.nhnacademy.edu.springframework;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
         User user = new User("aaaa@gmail.com", "010-2222-2222");
-
-
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
+            new MessageSendService(context.getBean("smsMessageSender", MessageSender.class)).doSendMessage(user, "hi");
+            new MessageSendService(context.getBean("emailMessageSender", MessageSender.class)).doSendMessage(user, "hi");
+        }
     }
 }
