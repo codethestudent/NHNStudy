82c46b706ec130aca2306d73ca1cef0dfa095064
codeTheStudent <hjun303@gnu.ac.kr>
2024-02-05 17:58:59 +0900

    files to be added

--- springmvc/src/main/com/nhnacademy/springmvc/config/RootConfig.java
+++ springmvc/src/main/com/nhnacademy/springmvc/config/RootConfig.java
@@ -1,2 +1,9 @@
-package com.nhnacademy.springmvc.config;public class RootConfig {
+package com.nhnacademy.springmvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.nhnacademy.springmvc"})
+public class RootConfig {
 }
--- springmvc/src/main/com/nhnacademy/springmvc/config/WebConfig.java
+++ springmvc/src/main/com/nhnacademy/springmvc/config/WebConfig.java
@@ -1,2 +1,23 @@
-package com.nhnacademy.springmvc.config;public class WebConfig {
+package com.nhnacademy.springmvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.nhnacademy.springmvc.**.controller")
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/",".jsp");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
 }
