da9e919680e6335c857ff5eb0a9900e785d93498
codeTheStudent <hjun303@gnu.ac.kr>
2024-02-20 18:36:23 +0900

    new project

---
+++ account-openapi/src/main/java/com/nhnacademy/edu/springboot/accountopenapi/AccountOpenapiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.edu.springboot.accountopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountOpenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountOpenapiApplication.class, args);
+	}
+
+}
