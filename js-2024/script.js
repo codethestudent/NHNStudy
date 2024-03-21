cafcf1ddaa69d1e620eb7556ba3163ebc8c26e6e
codeTheStudent <hjun303@gnu.ac.kr>
2024-03-21 09:17:10 +0900

    javascript added

---
+++ js-2024/script.js
@@ -0,0 +1,29 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     let loginBtn = document.getElementById("loginBtn");
+//
+//     loginBtn.addEventListener("click", function () {
+//         console.log("loginBtn click!");
+//     });
+// });
+
+// var today = new Date();
+// var a = 10; // 근본잉 없음, 걍 뭘해도 다됨
+// // var a=20; 이렇게 해도 된다는 거지
+// const b = 20;
+// let c = 30;
+//
+// function aaa() {
+//     'use strict';
+//
+// }
+
+// let username = "global";
+// {
+//     let username = "local";
+//     console.log("local", username);
+// }
+// console.log("global:", username);
+
+console.log("before", color);
+var color="red";
+console.log("after",color);
